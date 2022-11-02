08f4193f1e9eda0520a313b4ab0cfb0b8ab7d5ec
Henning Meyer <tutmann@gmail.com>
2022-11-02 08:35:35 +0100

    strip down deps

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -1,5 +1,4 @@
 use shadercanvas::ShaderCanvas;
-use js_sys;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
@@ -17,11 +16,6 @@ pub fn start() -> Result<(), JsValue> {
     let document = window.document().unwrap();
     let canvas = document.get_element_by_id("canvas").unwrap();
     let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
-    let cw = (window.inner_width().unwrap().as_f64().unwrap() * 0.8) as u32;
-    let ch = (window.inner_height().unwrap().as_f64().unwrap() * 0.8) as u32;
-    info!("setting canvas dimensions to [{}x{}].", cw, ch);
-    canvas.set_width(cw);
-    canvas.set_height(ch);
 
     let shader_canvas = ShaderCanvas::new(canvas.clone())?;
     let shader_canvas = Rc::new(shader_canvas);
@@ -29,7 +23,6 @@ pub fn start() -> Result<(), JsValue> {
     {
         let clone = shader_canvas.clone();
         let closure = Closure::<dyn FnMut(_)>::new(move |_event: web_sys::MouseEvent| {
-            info!("called event");
             clone.draw();
         });
         canvas.add_event_listener_with_callback("mousedown", closure.as_ref().unchecked_ref())?;
