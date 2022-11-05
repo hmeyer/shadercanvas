c72922b8849435c16abdc064de7877c7ad14ae76
Henning Meyer <tutmann@gmail.com>
2022-11-05 19:46:09 +0100

    cargo fmt

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -16,11 +16,13 @@ pub fn start() -> Result<(), JsValue> {
     shader_canvas.set_shader(shader_program)?;
     let closure = Closure::<dyn Fn()>::new(move || {
         shader_canvas.draw();
-    });        
-    
+    });
+
     let redraw_interval = std::time::Duration::from_millis(50);
     window.set_interval_with_callback_and_timeout_and_arguments_0(
-        closure.as_ref().unchecked_ref(), redraw_interval.as_millis() as i32)?;
+        closure.as_ref().unchecked_ref(),
+        redraw_interval.as_millis() as i32,
+    )?;
     closure.forget();
     Ok(())
 }
